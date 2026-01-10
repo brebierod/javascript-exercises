fb386fc2c7365d743aef7b84aaceb4c074dc99e7
brebierod <bierodbre@gmail.com>
2026-01-10 17:55:08 -0500

    Complete exercise 06 repeatStrings

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let newStr = "";
+    if (num < 0) {
+        return "ERROR"
+;    } else {
+  for (i = 0 ; i < num ; i++ ) {
+    newStr = str + newStr ;
+    }
+    return newStr; }
 };
 
+repeatString("hey", 3);
+repeatString("hello", 10);
+repeatString("hi", 1);
+repeatString("bye", 0);
+repeatString("bye", -1);
 // Do not edit below this line
 module.exports = repeatString;

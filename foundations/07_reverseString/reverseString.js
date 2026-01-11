3bf25e39202723df9e310173caf8574afcd25dde
brebierod <bierodbre@gmail.com>
2026-01-11 00:43:00 -0500

    Complete js exercise 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = (str) => {
+    let newStr = str;
+return newStr
+    .split('')
+    .reverse()
+    .join('');
 };
 
+reverseString("hello");
+reverseString('hello there')
+reverseString('123! abc! Hello, Odinite')
+reverseString(' ')
 // Do not edit below this line
 module.exports = reverseString;

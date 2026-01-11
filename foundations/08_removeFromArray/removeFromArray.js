6f52bb0a1f1f31aff445312fa037e21c13ce07b5
brebierod <bierodbre@gmail.com>
2026-01-11 14:38:24 -0500

    Redo exercise 08 with array methods

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,23 +1,13 @@
-const removeFromArray = (arr, a, b, c, d) => {
-    for (let i = 0; i < arr.length;) {
-        if (arr[i] === a || arr[i] == b || arr[i] == c 
-            || arr[i] == d 
-        ) {
-            arr.splice(i, 1);
-        }
-        else {
-            i++;
-        }
-    }      
-  return arr;
-};
+const removeFromArray = (arr, ...args) => {
+    return arr.filter((item) => !args.includes(item));
+}
 let test = [1, 2, 3, 3, 4];
 let test2 = ["hey", 2, 3, "ho"];
 removeFromArray(test, 3);
 removeFromArray(test, 3, 2);
 removeFromArray(test, 7, "tacos");
-removeFromArray(test, 7, 2, 3, 4);
-removeFromArray(test2,"hey", 3 );
-removeFromArray(test, "1"), 3;
+removeFromArray(test, 7, 2, 3,);
+removeFromArray(test2, "hey", 3);
+removeFromArray(test, "1", 3);
 // Do not edit below this line
 module.exports = removeFromArray;

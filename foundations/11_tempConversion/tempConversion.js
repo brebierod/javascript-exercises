d7af5a9d8c340a18013cf23fd476949aff72a07f
brebierod <bierodbre@gmail.com>
2026-01-13 16:31:59 -0500

    Complete temp conversion exercise

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,43 @@
-const convertToCelsius = function() {
+function decimalRound(num) {
+  const numStr = num.toString();
+  let rounded;
+  //numStr = "0.6234";
+  if (numStr.indexOf(".") === -1){
+    return num;
+  }
+  else {
+    let decimalPos = numStr.indexOf(".");
+    if (numStr[decimalPos + 2] >= 5) {
+      let rounded = numStr.slice(0,decimalPos + 1) + (+(numStr[decimalPos + 1]) + 1);
+      return rounded;
+    }
+    else if (numStr[decimalPos + 2] < 5) {
+      let rounded = numStr.slice(0,decimalPos + 1) + (+(numStr[decimalPos + 1]));
+      return rounded;
+     }
+    
+  }
+    return rounded;
+}
+
+const convertToCelsius = function(num) {
+//°C = (°F − 32) × 5/9
+let answer = (num - 32) * (5/9);
+if (answer >= 0)
+return Math.abs(decimalRound(answer));
+else {
+  return +decimalRound(answer);
+}
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  // °F = °C × 9/5 + 32
+  let answer = num * (9/5) + 32;
+if (answer >= 0)
+return Math.abs(decimalRound(answer));
+else {
+  return +decimalRound(answer);
+}
 };
 
 // Do not edit below this line

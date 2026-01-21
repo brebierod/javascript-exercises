f9439cc325437530507be72d428cd86f1cb113df
brebierod <bierodbre@gmail.com>
2026-01-21 01:09:09 -0500

    Complete findOldest and finish foundations

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function (arr) {
+    let oldest = arr.sort((a, b) => {
+        let personA = a.yearOfDeath - a.yearOfBirth;
+        let personB = b.yearOfDeath - b.yearOfBirth;
+        if (personB > personA || b.yearOfDeath === undefined && b.yearOfBirth < a.yearOfBirth) {
+            return 1;
+        } if (personB < personA || b.yearOfDeath === undefined && b.yearOfBirth > a.yearOfBirth) {
+            return -1;
+        } else {
+            return 0;
+        }
+    });
+    return oldest[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;

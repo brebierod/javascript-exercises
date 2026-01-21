0e33bcd4f158308a14335195663ddd10e56a3f3b
brebierod <bierodbre@gmail.com>
2026-01-20 23:50:54 -0500

    Complete palindrome exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
-};
+const palindromes = function (words) {
+    const lettersArray = words.toLowerCase()
+                                                    .split('');
+    let newArray = lettersArray.filter((item) => /[a-z]/i.test(item) === true || /\d/.test(item) === true);
+    let reverseArray = newArray.map((item) => item);
+    reverseArray.reverse();
+    if (reverseArray.join('') === newArray.join('')) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // Do not edit below this line
 module.exports = palindromes;

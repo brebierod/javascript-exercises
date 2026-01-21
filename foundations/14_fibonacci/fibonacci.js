a1bf225b95804e0fbe740a5ed68cc444b90c029b
brebierod <bierodbre@gmail.com>
2026-01-21 00:10:43 -0500

    Complete fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    let fibonacciArray = [0, 1, 1];
+    if (num < 0) {
+        return "OOPS";
+    } else {
+        for (let i = 2; i <= num; i++) {
+            fibonacciArray[i] = fibonacciArray[i - 2] + fibonacciArray[i - 1]
+        }
+        return fibonacciArray[num];
+    }
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
